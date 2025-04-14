0feffe7872896887f4ee81140a6244270de9e61d
brandtryan <emailme@brandtryan.com>
2025-04-13 20:33:27 -0400

    Continue at confused - which is page 1

---
+++ sandboxes/ch04Sandbox.js
@@ -0,0 +1,7 @@
+const { curry } = require('@mostly-adequate/support');
+
+const match = curry((what, s) => s.match(what));
+const replace = curry((what, replacement, s) => s.replace(what, replacement));
+const filter = curry((f, xs) => xs.filter(f));
+const map = curry((f, xs) => xs.map(f));
+console.log(match.constructor);
